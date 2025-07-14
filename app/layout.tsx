93746f7d10745199d15290edccb8f9d79763d09a
pevesindoit <guntek2000@gmail.com>
2025-07-14 09:32:39 +0800

    fead: analythicts

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import { Inter } from "next/font/google";
 import "./globals.css";
 import NavBar from "./component/NavBar";
 import 'leaflet/dist/leaflet.css';
+import { Analytics } from "@vercel/analytics/next"
 
 const inter = Inter({
   subsets: ["latin"],
@@ -23,6 +24,7 @@ export default function RootLayout({
       <body
         className={`${inter.className} antialiased bg-[#F9FAFB]`}
       >
+        <Analytics />
         <NavBar />
         {children}
       </body>
